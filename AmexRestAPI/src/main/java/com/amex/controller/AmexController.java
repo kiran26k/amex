390c79f22b2728c9a2aea7c2fe39c9badcacdf38
kirankrk <kiran123>
2018-08-06 23:08:23 +0530

    Wire mocking test

--- AmexRestAPI/src/main/java/com/amex/controller/AmexController.java
+++ AmexRestAPI/src/main/java/com/amex/controller/AmexController.java
@@ -5,25 +5,28 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.amex.model.GUID;
 import com.amex.service.AmexService;
 
 
 
-@Controller
+@RestController
 public class AmexController {
 	
 	@Autowired
 	AmexService amexService;
 
-	 
-	 @GetMapping(path ="/amexsite/v1/corporate_accounts/tokens/{token_number}/", produces=MediaType.APPLICATION_JSON_VALUE)
-	 public GUID getGUIDbyToken(@PathVariable String token) {
+	 ///amexsite/v1/corporate_accounts/tokens/{token_number}/
+	 @GetMapping(path ="/amexsite/v1/corporate_accounts/tokens/{token_number}", produces = MediaType.APPLICATION_JSON_VALUE)
+	 public GUID getGUIDbyToken(@PathVariable("token_number") String token) {
 		 //Call service giving GUID using token
 		 
 		 System.out.println("getGUIDbyToken");
 		 GUID guidModel = amexService.getGUIDbyTokenService(token);
+		 System.out.println(guidModel);
+		 System.out.println(MediaType.APPLICATION_JSON_VALUE);
 		 return guidModel;
 	 }
 
--- AmexRestAPI/src/test/java/com/amex/controller/AmexControllerTest.java
+++ AmexRestAPI/src/test/java/com/amex/controller/AmexControllerTest.java
@@ -22,14 +22,14 @@ import com.github.tomakehurst.wiremock.junit.WireMockRule;
 @RunWith(SpringJUnit4ClassRunner.class)
 @AutoConfigureMockMvc
 @ActiveProfiles(value = "integration")
-@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AmexControllerTest {
 
 	RestTemplate restTemplate;
 	
 
 	@Rule
-	public WireMockRule wireMockRule = new WireMockRule(8081);
+	public WireMockRule wireMockRule = new WireMockRule(8080);
 	
 
 	@Before
@@ -41,22 +41,13 @@ public class AmexControllerTest {
 	public void testLocalServiceWithMockedRemoteService() throws Exception {
 		
 		restTemplate = new RestTemplate();
-		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8081/amex", String.class);
-		String expected = "SampleResponse: {\r\n" + 
-				"  \"getGUIDbyTokenResponse\": {\r\n" + 
-				"    \"Response\": {\r\n" + 
-				"      \"RespCd\": \"0\",\r\n" + 
-				"      \"getGUIDbyTokenResp\": {\r\n" + 
-				"        \"GUID\": \"e82662gvebdjdi987277\"\r\n" + 
-				"      }\r\n" + 
-				"    }\r\n" + 
-				"  }\r\n" + 
-				"}";
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8081/amex/amexsite/v1/corporate_accounts/tokens/3", String.class);
+		String expected = "{\"getGUIDbyTokenResponse\":{\"response\":null}}";
 		JSONAssert.assertEquals(expected, response.getBody(), true);
 	}
 
 	private void mockGUIDService() {	
-		WireMock.stubFor(WireMock.get(WireMock.urlMatching("/amexsite/v1/corporate_accounts/tokens/123/"))
+		WireMock.stubFor(WireMock.get(WireMock.urlMatching("/amexsite/v1/corporate_accounts/tokens/123"))
 	            .willReturn(
 	                    WireMock.aResponse()
 	                            .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_UTF8_VALUE)
