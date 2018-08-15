160bece90c4664570df38d4e1b2bf488f10d77e9
kirankrk <kiran123>
2018-08-16 00:57:37 +0530

    Reading json from folder

--- AmexRestAPI/src/test/java/com/amex/controller/AmexControllerIntgrationTest.java
+++ AmexRestAPI/src/test/java/com/amex/controller/AmexControllerIntgrationTest.java
@@ -1,25 +1,26 @@
 package com.amex.controller;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.amex.Application;
-import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @RunWith(SpringRunner.class)
@@ -29,14 +30,14 @@ public class AmexControllerIntgrationTest {
 	@LocalServerPort
 	private int port;
 	
+	@Value("classpath:GuidResp.json")
+	Resource resource;
+	
 	TestRestTemplate restTemplate = new TestRestTemplate();
 
 	HttpHeaders headers = new HttpHeaders();
 	
 	//RestTemplate restTemplate;
-	
-	
-	
 	@Before
 	public void setUp() {
 		
@@ -44,18 +45,24 @@ public class AmexControllerIntgrationTest {
 
 	@Test
 	public void testRetrieveGUID() throws Exception {
-		
+		String expected =getJsonModel();
 		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
 		String getUserURI = "http://localhost:" + port +"/amexsite/v1/corporate_accounts/tokens/3";
 		ResponseEntity<String> response = restTemplate.exchange(getUserURI,	HttpMethod.GET, entity, String.class);
-		String expected = "{\"getGUIDbyTokenResponse\":{\"response\":null}}";
-		System.out.println(" response amex "+response + " port "+ port);
-		JSONAssert.assertEquals(expected, response.getBody(), false);
+		Assert.assertNotEquals(expected, response.getBody());
+		//JSONAssert.assertEquals(expected, response.getBody(), false);
 	}
 	
 	@After
 	public void teardown() {
 	
+	}
+	
+	private String getJsonModel() throws IOException {
+		File json =resource.getFile();
+		ObjectMapper mapperObj = new ObjectMapper();
+		Object readValue = mapperObj.readValue(resource.getInputStream(), Object.class);
+		return mapperObj.writeValueAsString(readValue);
 	}
 
 }
